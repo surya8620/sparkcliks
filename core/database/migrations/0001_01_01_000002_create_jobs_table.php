5e89b589e16c08895ac5ff7ac83e592f831fb402
surya <surya.s.rao@gmail.com>
2026-05-15 13:37:52 +0530

    chore: guard default Laravel framework migrations with hasTable checks

--- core/database/migrations/0001_01_01_000002_create_jobs_table.php
+++ core/database/migrations/0001_01_01_000002_create_jobs_table.php
@@ -11,6 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
+        if (Schema::hasTable('jobs')) return;
+
         Schema::create('jobs', function (Blueprint $table) {
             $table->id();
             $table->string('queue')->index();
